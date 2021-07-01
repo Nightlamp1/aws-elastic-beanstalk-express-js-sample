6494aa224cfa5f85fb05b8a2ea0e4f6d92eb5c53
unknown <robert.britt2011@gmail.com>
2021-07-01 16:48:46 -0500

    Updating message v2.0

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('What up everyone knows Jose is the cooliest'));
+app.get('/', (req, res) => res.send('First AWS Setup for auto build/deploy... WHAT UP HANK!?'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
